960360e89cfdd1a10c3e4472656e4e4491289df2
Philip Culverston <philip.culverston@protonmail.ch>
2020-05-16 23:27:38 +0100

    Excercise 12

---
+++ Excercise_12.py
@@ -0,0 +1,7 @@
+#Defining 3 variables and asking for the user input along with a print message.
+age = input ("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+# prints out the line.
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
