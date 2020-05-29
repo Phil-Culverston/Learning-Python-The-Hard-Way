cf37aac256c86908b7291cbcb53b29f4f1f7ce81
Philip Culverston <philip.culverston@protonmail.ch>
2020-05-30 00:04:04 +0100

    Updated with drill challenges.

--- Excercise_12.py
+++ Excercise_12.py
@@ -1,7 +1,11 @@
-#Defining 3 variables and asking for the user input along with a print message.
-age = input ("How old are you?")
+# Defining 3 variables and asking for the user input along with a print message.
+age = input("How old are you?")
 height = input("How tall are you?")
 weight = input("How much do you weigh?")
+luckynum = ("What is your lucky number?",input()) # Testing a funky prompt.
 
-# prints out the line.
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+# Prints out the line.
+print(f"""
+So, you're {age} old, {height} tall and {weight} heavy. and your
+lucky number is {luckynum}
+""") # luckynum will fail to prompt the user. As stated this is a funky prompt.
