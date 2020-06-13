c2d0f5e6ab782514e5e2c2bc442e8b574501cdc0
Philip Culverston <philip.culverston@protonmail.ch>
2020-06-13 02:36:03 +0100

    Excercise_15 Study Drill Complete

--- Excercise_15.py
+++ Excercise_15.py
@@ -1,4 +1,5 @@
-from sys import argv # Imports argv so we can choose the file to open.
+# Imports argv so we can choose the file to open.
+from sys import argv
 
 # Setting the argv parameters as script and filename. Then opens the file.
 script,filename = argv
@@ -7,13 +8,19 @@ txt = open(filename)
 # Prints the filename to the user then reads out the content of the file.
 print(f"Here's your file {filename}:")
 print(txt.read())
+# * Always use close after opening file as to disgard unneeded resources.
+txt.close()
 
 # Asks the user if they wish to look at another file.
 print("Type the filename again:")
 file_again = input(">")
 
 # opens the file requested by user.
-txt_again = open(file_again)
+txt.again = open(file_again)
 
 # Reads out the file.
 print(txt_again.read())
+txt.again.close()
+
+# * - All comments marked with an asterisk are important for code optimisation.
+# Messy comments on this one but full of important litte details :D
